987903a4653801ff2a8a8f46f480eee7270bc4aa
Trần Quốc Tuấn <tranquoctuantqd2002@gmail.com>
2023-02-07 11:40:31 +0700

    update RelativeLayoutTest

---
+++ firstapplication/app/src/main/java/hcmute/edu/vn/firstapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package hcmute.edu.vn.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relativelayouttest);
+
+//        button = findViewById(R.id.buttonClick);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Chào mừng bạn đến với Android", Toast.LENGTH_LONG ).show();
+//            }
+//        });
+    }
+}
